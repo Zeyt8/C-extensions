b20867f46d7be2879c39cd91fb7a7cae67af212c
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-06-01 01:23:31 +0300

    Refactor tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
+build() {
+    make all
+}
+
+run() {
+    for test in *_tests; do
+        if ! ./$test; then
+            echo "$test failed."
+            exit 1
+        fi
+    done
+}
+
+clean() {
+    make clean
+}
+
 if [ "$1" != "string" ] && [ "$1" != "list" ]; then
-    echo "Usage: $0 {string|list}"
+    echo "Usage: $0 {string|list} {build|run|clean|all}"
     exit 1
 fi
 
 cd "$1"
 
-make all
-make run
-make clean
+case $2 in
+    build)
+        build
+        ;;
+    run)
+        run
+        ;;
+    clean)
+        clean
+        ;;
+    all)
+        build
+        run
+        clean
+        ;;
+    *)
+        echo "Invalid command. Usage: $0 {string|list} {build|run|clean|all}"
+        exit 1
+        ;;
+esac
 
 cd ..
